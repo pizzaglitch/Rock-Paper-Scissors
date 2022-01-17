1420ce8aedaba42d5cd2954531561700ea54d57a
Garrett Hufstedler <garretthufstedler@gmail.com>
2022-01-17 17:19:50 -0500

    Added computer choice for Rock, Paper, Scissors

---
+++ script.js
@@ -0,0 +1,13 @@
+function computerPlay () {
+    let randomNumber = Math.floor(Math.random() * 3); 
+    if (randomNumber === 0) {
+        return "Rock";
+    }
+    else if (randomNumber === 1) {
+        return "Paper";
+    }
+    else if (randomNumber === 2) {
+        return "Scissors";
+    }
+}
+console.log(computerPlay());
