cb7cd67cc1f7aea686190263d46d077bb4d0843f
Garrett Hufstedler <garretthufstedler@gmail.com>
2022-01-17 19:29:38 -0500

    Created computer versus user function using if..else statements.

--- script.js
+++ script.js
@@ -10,4 +10,37 @@ function computerPlay () {
         return "Scissors";
     }
 }
-console.log(computerPlay());
+
+function playRound(playerSelection, computerSelection) {
+var input = playerSelection.toLowerCase()
+if (input === computerSelection) {
+    return "It's a tie."; 
+} 
+else if (input === "rock") {
+    if (computerSelection === "Scissors") {
+            return "You win ! Rock beats scissors.";
+            }
+        else {
+            return "You lose. Paper beats rock.";
+         }
+    } 
+else if (input === "paper") {
+    if (computerSelection === "Rock") {
+            return "You win! Paper beats rock.";
+            } 
+        else {
+            return "You lose. Scissors beats paper.";
+        }
+    } 
+else if (input === "scissors") {
+    if (computerSelection === "Paper") {
+            return "You win! Scissors beats paper.";
+            }
+        else {
+            return "You lose. Rock beats scissors."
+        }
+    }
+}
+const playerSelection = "scissors";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
