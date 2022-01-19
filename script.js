b3a29000661e2c005a542b43c76faea51389598c
Garrett Hufstedler <garretthufstedler@gmail.com>
2022-01-18 20:44:07 -0500

    Added game function and winner function.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0; 
+let computerScore = 0; 
+let draws = 0; 
 function computerPlay () {
     let randomNumber = Math.floor(Math.random() * 3); 
     if (randomNumber === 0) {
@@ -11,36 +14,60 @@ function computerPlay () {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-var input = playerSelection.toLowerCase()
+function playRound() {
+const playerSelection = prompt("Choose your weapon: Rock, Paper, or Scissors.");
+const computerSelection = computerPlay();
+const input = playerSelection.toLowerCase()
 if (input === computerSelection) {
     return "It's a tie."; 
 } 
 else if (input === "rock") {
     if (computerSelection === "Scissors") {
-            return "You win ! Rock beats scissors.";
-            }
-        else {
-            return "You lose. Paper beats rock.";
-         }
+        playerScore +=1;
+        return "You win ! Rock beats scissors. Player has " + playerScore + " point(s).";
+        } else {
+        computerScore +=1;
+        return "You lose. Paper beats rock. Computer has " + computerScore + " point(s).";
+        }
     } 
 else if (input === "paper") {
     if (computerSelection === "Rock") {
-            return "You win! Paper beats rock.";
-            } 
-        else {
-            return "You lose. Scissors beats paper.";
+        playerScore +=1;
+        return "You win! Paper beats rock. Player has " + playerScore + " point(s).";
+        } else {
+        computerScore +=1;
+        return "You lose. Scissors beats paper. Computer has " + computerScore + " point(s).";
         }
     } 
 else if (input === "scissors") {
     if (computerSelection === "Paper") {
-            return "You win! Scissors beats paper.";
-            }
-        else {
-            return "You lose. Rock beats scissors."
+        playerScore +=1;
+        return "You win! Scissors beats paper. Player has " + playerScore + " point(s).";
+        } else {
+        computerScore +=1;
+        return "You lose. Rock beats scissors. Computer has " + computerScore + " point(s)."
         }
     }
 }
-const playerSelection = "scissors";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+
+function winner() {
+    if (playerScore >= 3) {
+    return "Player wins."; 
+} else  { (computerScore >= 3) 
+    return "Computer wins.";
+}
+}
+    
+function game() {
+    console.log(playRound() + " First round complete.");
+    console.log(playRound() + " Second round complete.");
+    console.log(playRound() + " Third round complete.");
+    console.log(playRound() + " Fourth round complete.");
+    console.log(playRound() + " Fifth round complete.");
+    console.log(winner());
+}
+
+game();
+
+
+
