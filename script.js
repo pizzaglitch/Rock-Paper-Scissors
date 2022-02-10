0a6fa158f556c58970c006b044fbb8039355490a
Garrett Hufstedler <garretthufstedler@gmail.com>
2022-02-09 20:15:49 -0500

    Added new div and function to display winner

--- script.js
+++ script.js
@@ -17,36 +17,34 @@ function playRound() {
 // const playerSelection = prompt("Choose your weapon: Rock, Paper, or Scissors.");
 const computerSelection = computerPlay();
 // const input = playerSelection.toLowerCase(); 
-document.getElementById('results').textContent = "Computer: " + computerSelection; 
-
 if (input === computerSelection) {
     return "It's a tie."; 
 } 
 else if (input === "rock") {
     if (computerSelection === "scissors") {
         playerScore +=1;
-        return "You win ! Rock beats scissors. Player has " + playerScore + " point(s).";
+        return "You win ! Rock beats scissors.";
         } else {
         computerScore +=1;
-        return "You lose. Paper beats rock. Computer has " + computerScore + " point(s).";
+        return "You lose. Paper beats rock.";
         }
     } 
 else if (input === "paper") {
     if (computerSelection === "rock") {
         playerScore +=1;
-        return "You win! Paper beats rock. Player has " + playerScore + " point(s).";
+        return "You win! Paper beats rock.";
         } else {
         computerScore +=1;
-        return "You lose. Scissors beats paper. Computer has " + computerScore + " point(s).";
+        return "You lose. Scissors beats paper.";
         }
     } 
 else if (input === "scissors") {
     if (computerSelection === "paper") {
         playerScore +=1;
-        return "You win! Scissors beats paper. Player has " + playerScore + " point(s).";
+        return "You win! Scissors beats paper.";
         } else {
         computerScore +=1;
-        return "You lose. Rock beats scissors. Computer has " + computerScore + " point(s).";
+        return "You lose. Rock beats scissors.";
         }
     }
 
@@ -63,10 +61,13 @@ while (isValid==false){
 }
 */
 }
-function printResults(playRound) {
-    document.getElementById('results').textContent = playRound;
-    console.log(playRound);
-}
+//document.getElementById('results').textContent = 
+
+// possible solution to print playRound to div 
+//  function printResults(playRound) {
+//      document.getElementById('results').textContent = playRound;
+//      console.log(playRound);
+//  }
 
 function winner() {
     if (playerScore > computerScore) {
@@ -78,33 +79,31 @@ function winner() {
 }
 }
 
+
 //event listener for button to run playRound function
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         input = button.id;
-        console.log(playRound(input));
+        document.getElementById('results').textContent = playRound(input) + ` The score is Player: ${playerScore}, Computer: ${computerScore}.`;
+        if (playerScore >= 5 || computerScore >= 5) {
+        document.getElementById('winner').textContent = winner();
+        }
+        
     });
 });
 
 
-// possible solution to output round result to div 
-// const results = document.querySelector('#results');
-// const resultEachRound = document.createElement('div');
-// content.textContent = console.log(playground);
-// resultEachRound.classList.add('resultEachRound');
-// results.appendChild(resultEachRound);
 
 
 
-// function game() { --removing temporarily--
-//     console.log(playRound() + " First round complete.");
+ //function game() { 
+ //    playRound + " First round complete.";
 //     console.log(playRound() + " Second round complete.");
 //     console.log(playRound() + " Third round complete.");
 //     console.log(playRound() + " Fourth round complete.");
 //     console.log(playRound() + " Fifth round complete.");
 //     console.log(winner());
-     
-// }
+ // }
 
-//game();
+// game()
